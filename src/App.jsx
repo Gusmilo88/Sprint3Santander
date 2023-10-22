e9023026c570f312af1ed3728c3197f4c0276755
Gus <gus_eze1988@hotmail.com>
2023-10-22 19:23:56 -0300

    searchbar terminado

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import Carousel from './components/Carousel/Carousel'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import SearchBar from './components/SearchBar/index.'
 
 function App() {
 
   return (
     <>
       <Header/>
-      <Carousel></Carousel>
+      <Carousel/>
+      <SearchBar/>
       <Footer/>
     </>
   )
