613f09611c73ed0cca73692b18c826b929f564d5
Gus <gus_eze1988@hotmail.com>
2023-10-24 10:36:56 -0300

    formcontact hecho componente

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -8,7 +8,6 @@ const Layouts = ({children}) => {
     <>
       <Header/>
       <Carousel/>
-      <VistaSelector/>
       {children}
       <Footer/>
     </>
--- src/pages/Stats/index.jsx
+++ src/pages/Stats/index.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React from "react";
+import Layouts from "../../layouts";
 
 const Stats = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <Layouts>
+        {/* Tabla Events statistics */}
+        <table className="table table-dark table-striped">
+          <thead>
+            <tr>
+              <th colSpan={4}>Events statistics</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+          </tbody>
+        </table>
 
-export default Stats
+        {/* Tabla Events statistics */}
+        <table className="table table-dark table-striped">
+          <thead>
+            <tr>
+              <th colSpan={4}>Events statistics</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+          </tbody>
+        </table>
+
+        {/* Tabla Events statistics */}
+        <table className="table table-dark table-striped">
+          <thead>
+            <tr>
+              <th colSpan={4}>Events statistics</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+          </tbody>
+        </table>
+      </Layouts>
+    </>
+  );
+};
+
+export default Stats;
