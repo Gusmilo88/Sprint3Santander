1a8f6b8db33a9e544d77d2a43254010bc7390108
Gus <gus_eze1988@hotmail.com>
2023-10-26 11:36:19 -0300

    agrega rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import Contact from "./pages/Contact"
 import Home from "./pages/Home"
 import Stats from "./pages/Stats"
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom"
 
 function App() {
 
   return (
     <>
-      <Home/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/stats" element={<Stats/>}/>
+          {/* <Route path="*" element={<404NotFound/>}/> */}
+        </Routes>
+      </Router>
+
+      
     </>
   )
 }
