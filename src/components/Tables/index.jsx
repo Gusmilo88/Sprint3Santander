ff698871e6819a8000f94584a212e079c8b2d270
Gus <gus_eze1988@hotmail.com>
2023-11-05 21:24:01 -0300

    Tablas terminadas

--- src/components/Tables/index.jsx
+++ src/components/Tables/index.jsx
@@ -5,20 +5,20 @@ const Tables = ({ events, highestAttendanceEvent, highestAttendancePercentage,lo
   return (
     <div role="tables">
       {/* Tabla Events */}
-      <div className="my-5">
+      <div className="text-center my-5">
       <table className="table table-dark table-striped">
         <thead>
           <tr>
-            <th colSpan={3} className="text-center text-3xl">Events statistics</th>
+            <th colSpan={3} className="text-3xl">Events statistics</th>
           </tr>
-          <tr className="text-center">
+          <tr>
             <th>With the highest % of assistance:</th>
             <th>With the lowest % of assistance:</th>
             <th>With the largest capacity:</th>
           </tr>
         </thead>
         <tbody>
-          <tr className="text-center">
+          <tr>
             <td>{highestAttendanceEvent ? highestAttendanceEvent.name : "No hay infomarción disponible"}: {highestAttendancePercentage.toFixed(2)} %</td>
             <td>{lowestAttendanceEvent ? lowestAttendanceEvent.name : "No hay información disponible"}: {lowestAttendancePercentage.toFixed(2)} % </td>
             <td>{largestCapacityEvent ? largestCapacityEvent.name : "No hay información disponible"}: {largestCapacity}</td>
@@ -27,13 +27,13 @@ const Tables = ({ events, highestAttendanceEvent, highestAttendancePercentage,lo
       </table>
       </div>
 
-      <div className="my-5">
+      <div className="text-center my-5">
       <table className="table table-dark table-striped">
         <thead>
           <tr>
-            <th colSpan={3} className="text-center text-3xl">Upcoming events statistics by category</th>
+            <th colSpan={3} className="text-3xl">Upcoming events statistics by category</th>
           </tr>
-          <tr className="text-center">
+          <tr>
             <th>Categories:</th>
             <th>Revenues:</th>
             <th>% of attendance:</th>
@@ -41,7 +41,7 @@ const Tables = ({ events, highestAttendanceEvent, highestAttendancePercentage,lo
         </thead>
         <tbody>
         {upcomingEvents.map(event => (
-        <tr key={event._id} className="text-center">
+        <tr key={event._id}>
           <td>{event.category}</td>
           <td>$ {((event.assistance || event.estimate) * event.price).toFixed(2)}</td>
           <td>{((event.assistance || event.estimate) / event.capacity * 100).toFixed(2)} %</td>
@@ -52,13 +52,13 @@ const Tables = ({ events, highestAttendanceEvent, highestAttendancePercentage,lo
       </div>
 
 
-      <div className="my-5">
+      <div className="text-center my-5">
       <table className="table table-dark table-striped">
         <thead>
           <tr>
-            <th colSpan={3} className="text-center text-3xl">Past events statistics by category</th>
+            <th colSpan={3} className="text-3xl">Past events statistics by category</th>
           </tr>
-          <tr className="text-center">
+          <tr>
             <th>Categories:</th>
             <th>Revenues:</th>
             <th>% of attendance:</th>
@@ -66,7 +66,7 @@ const Tables = ({ events, highestAttendanceEvent, highestAttendancePercentage,lo
         </thead>
         <tbody>
       {pastEvents.map(event => (
-        <tr key={event._id} className="text-center">
+        <tr key={event._id}>
           <td>{event.category}</td>
           <td>$ {((event.assistance || event.estimate) * event.price).toFixed(2)}</td>
           <td>{((event.assistance || event.estimate) / event.capacity * 100).toFixed(2)} %</td>
