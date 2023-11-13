5d9e7edbb08d345e8cb111be387c1add4df33c3b
Gus <gus_eze1988@hotmail.com>
2023-11-12 22:13:24 -0300

    soluciona un link en el nav mobile

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -95,7 +95,7 @@ const Nav = () => {
               {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
               <Link
                 id="iconsNavMobile"
-                to={"/home"}
+                to={"/"}
                 className="text-white block rounded-md px-3 py-2 text-base font-medium"
               >
                 <i className="fa-solid fa-house" style={{color: "#ffffff"}}></i>
